c6352d94ad3d79125bc0ea61726d5bbd060c4655
pep-shock <945179695@qq.com>
2021-12-01 17:28:44 +0800

    update

---
+++ src/main/java/com/controller/IndexController.java
@@ -0,0 +1,15 @@
+package com.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+
+    @RequestMapping("/")
+    public String index() {
+        //设置视图名称
+        return "index";
+    }
+
+}

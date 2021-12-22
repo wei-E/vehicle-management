606ca28564b8cfb3e1f5b71f6765a24f631a444c
yanqichen <1571853251@qq.com>
2021-12-22 19:28:20 +0800

    更新sql语句

--- src/main/java/com/mapper/CarSendmapper.java
+++ src/main/java/com/mapper/CarSendmapper.java
@@ -7,6 +7,7 @@ import java.util.List;
 public interface CarSendmapper {
     List<CarSend> carSend_selectAll();
     List<CarSend> carSend_selectByCondition(CarSend carSend);
+    List<CarSend> carSend_selectViolations(CarSend carSend);
     String carSend_selectLicense(CarSend carSend);
     void carSend_add(CarSend carSend);
     void carSend_updateReturn(CarSend carSend);
